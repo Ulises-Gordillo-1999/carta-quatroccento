583039e74d9e5ba6d43a793f4a7cef49af76932f
Ulises Gordillo <uligordillo@gmail.com>
2024-03-21 00:13:28 -0300

    Se formateo el codigo, dejandolo mas legible

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,24 +1,113 @@
-import React from 'react'
+import React from "react";
 
-export default function Navbar({clickCategoria}) {
-
+export default function Navbar({ clickCategoria }) {
   return (
-    <div className='flex justify-around gap-2'>
-      <button onClick={clickCategoria} value={'entradas'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Entradas</button>
-      <button onClick={clickCategoria} value={'pescado'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Pescado</button>
-      <button onClick={clickCategoria} value={'carnes'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Carnes</button>
-      <button onClick={clickCategoria} value={'aves'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Aves</button>
-      <button onClick={clickCategoria} value={'cerdos'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Cerdos</button>
-      <button onClick={clickCategoria} value={'pastas'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Pastas</button>
-      <button onClick={clickCategoria} value={'guarnicion'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Guarnicion</button>
-      <button onClick={clickCategoria} value={'ensaladas'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Ensaladas</button>
-      <button onClick={clickCategoria} value={'recomendado-chef'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Sugerencia de Nuestro Chef</button>
-      <button onClick={clickCategoria} value={'vinos-tintos'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>VinosTintos</button>
-      <button onClick={clickCategoria} value={'vinos-blancos'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Vinos Blancos</button>
-      <button onClick={clickCategoria} value={'champagne'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Champagne</button>
-      <button onClick={clickCategoria} value={'gaseosas'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Gaseosas</button>
-      <button onClick={clickCategoria} value={'fernet'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Fernet</button>
-      <button onClick={clickCategoria} value={'postres'} className='h-12 w-full bg-slate-100 hover:bg-slate-300'>Postres</button>
+    <div className="flex flex-row w-full sm:justify-between md:justify-between lg:justify-between xl:justify-between justify-center items-center text-center mx-2 px-2">
+      <button
+        onClick={clickCategoria}
+        value={"entradas"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Entradas
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"pescado"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Pescado
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"carnes"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Carnes
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"aves"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Aves
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"cerdos"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Cerdos
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"pastas"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Pastas
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"guarnicion"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Guarnicion
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"ensaladas"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Ensaladas
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"recomendado-chef"}
+        className="w-4/5 text- sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white p-2 mt-4"
+      >
+        Sugerencia de Nuestro Chef
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"vinos-tintos"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        VinosTintos
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"vinos-blancos"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Vinos Blancos
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"champagne"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Champagne
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"gaseosas"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Gaseosas
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"fernet"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Fernet
+      </button>
+      <button
+        onClick={clickCategoria}
+        value={"postres"}
+        className=" w-full sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg bg-slate-100 rounded-lg hover:transition hover:scale-125 hover:bg-[#79093a] hover:text-white"
+      >
+        Postres
+      </button>
     </div>
-  )
+  );
 }
