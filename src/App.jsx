a9584b90eb4b5511d44868c5cd3f5a6dd42df69d
Ulises Gordillo <uligordillo@gmail.com>
2024-03-24 14:32:00 -0300

    Footer agregado en toda la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Carta from './Carta.jsx'
-import './index.css'
+import Carta from "./Carta.jsx";
+import Footer from './Footer.jsx'
+import "./index.css";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Carta/>} />
+        <Route path="/" element={<Carta />} />
       </Routes>
     </BrowserRouter>
   );
